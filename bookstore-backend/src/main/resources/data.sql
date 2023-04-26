a46e57275a8d6aab75a215d119d7f7410216cd70
darkoo59 <darko.selakovic11@gmail.com>
2023-04-26 22:57:21 +0200

    Add database initialization using SQL script

---
+++ bookstore-backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO book (id, title, author, publisher, number_of_pages, genre) VALUES
+(1,'The Great Gatsby', 'F. Scott Fitzgerald', 'Scribner', 180, 0),
+(2,'To Kill a Mockingbird', 'Harper Lee', 'J. B. Lippincott & Co.', 281, 1),
+(3,'1984', 'George Orwell', 'Secker & Warburg', 328, 2),
+(4,'Na Drini cuprija', 'Ivo Andric', 'Visegrad doo', 366, 3);
